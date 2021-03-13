b0c304325803bef8c8d8f16fe7ac412bfdadb8f8
Rinat <rinat89.2001@yandex.ru>
2021-03-13 17:00:20 +0300

    check successfull

--- lab2/laba1.java
+++ lab2/laba1.java
@@ -6,7 +6,7 @@
  * Калькулятор рабоатет с двумя переменными и можешь выводить на консоль либо возвращать результат
 */
 /*
-Fork chek
+Fork check123
 */
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
